4248095bde76b2cdf1410176a77adccea5a9ec9d
Bruce <bruce.hillsworth@gmail.com>
2023-03-24 10:41:43 -0600

    Pass tests on index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+function findMatching(array, name) {
+	const match = array.filter((element) => {
+		return element.toLowerCase() === name.toLowerCase();
+	});
+	return match;
+}
+
+function fuzzyMatch(array, name) {
+	const match = array.filter((element) => {
+		return (
+			element.slice(0, 2).toLowerCase() === name.slice(0, 2).toLowerCase()
+		);
+	});
+	return match;
+}
+
+function matchName(array, name) {
+	const match = array.filter((element) => {
+		return element.name === name;
+	});
+	return match;
+}
